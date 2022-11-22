4dd34063b9a9b23697d3bbe46fa52783aec8da33
Vladislav <98421574+Sicilianii@users.noreply.github.com>
2022-11-23 00:05:21 +0300

    js

--- apple/js/main.js
+++ apple/js/main.js
@@ -1,6 +1,6 @@
-function CallField() {
+function CallField(formClass, inputClass) {
     const regexp = new RegExp(/^[а-я]+/ig);
-    const inptForm = document.querySelectorAll('.footer-calls-form-inpt');
+    const inptForm = document.querySelectorAll(inputClass);
     inptForm.forEach(el => {
         el.addEventListener('input', event => {
             let inpt = event.target;
@@ -11,31 +11,50 @@ function CallField() {
             }
         });
     });
-    document.querySelector('.footer-calls-form').addEventListener('submit', event => {
+    const form = document.querySelector(formClass);
+    form.addEventListener('submit', event => {
         inptForm.forEach(el => {
             if(el.classList.contains('error')) {
                 event.preventDefault();
             }
-        })
+            if (!form.querySelectorall('input').forEach(el => el.classList.contains('error'))) {
+                form.classList.remove('visible');
+            }
+        });
     });
 }
 
-CallField();
+
 
 function request() {
-    const inptName = document.querySelector('#name-inpt');
-    const inptNumber = document.querySelector('#num-inpt');
+    const inptName = document.querySelector('#item-sales');
+    const inptNumber = document.querySelector('#item-price');
     const form = document.querySelector('.task');
-    const items = document.querySelectorAll('.catalog-item');
     const btnItems = document.querySelector('.catalog').querySelectorAll('button');
     btnItems.forEach(el => {
         el.addEventListener('click', event => {
             form.classList.add('visible');
+            console.log(inptName.value);
             inptName.value = event.target.parentElement.children[0].textContent;
             inptNumber.value = event.target.parentElement.children[1].textContent;
         });
     });
-    
+    document.querySelector('#light-cross').addEventListener('click', () => {
+        form.classList.remove('visible');
+    })
+    form.addEventListener('submit', event => {
+        let pop = form.querySelectorAll('.footer-calls-form-inpt').forEach(el => {
+            if(el.classList.contains('error')) {
+                event.preventDefault();
+            } 
+            if(el.value === '') {
+                event.preventDefault();
+                el.classList.add('error');
+            }
+        });
+    });
+    CallField('.task', '.footer-calls-form-inpt');
 }
 
+CallField('.footer-calls-form', '.footer-calls-form-inpt');
 request();

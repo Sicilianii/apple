2996d758159dbc46e5bd30288420a546fd499838
Vladislav <98421574+Sicilianii@users.noreply.github.com>
2022-11-22 21:18:00 +0300

    js

--- apple/js/main.js
+++ apple/js/main.js
@@ -1,19 +1,41 @@
 function CallField() {
-    const inptName = document.querySelector('#inpt-name');
-    const inptNumber = document.querySelector('#inpt-num');
     const regexp = new RegExp(/^[а-я]+/ig);
+    const inptForm = document.querySelectorAll('.footer-calls-form-inpt');
+    inptForm.forEach(el => {
+        el.addEventListener('input', event => {
+            let inpt = event.target;
+            if (inpt.value.match(regexp) === null) {
+            inpt.classList.add('error');
+            } else {
+            inpt.classList.remove('error');
+            }
+        });
+    });
     document.querySelector('.footer-calls-form').addEventListener('submit', event => {
-        if (inptName.value.match(regexp) === null) {
-            inptName.style.border = '2px solid red';
-            event.preventDefault();
-        } 
-        if (inptNumber.value.match(regexp) === null) {
-            inptName.style.border = '2px solid red';
-            event.preventDefault();
-        } else {
-            return;
-        }
+        inptForm.forEach(el => {
+            if(el.classList.contains('error')) {
+                event.preventDefault();
+            }
+        })
     });
 }
 
 CallField();
+
+function request() {
+    const inptName = document.querySelector('#name-inpt');
+    const inptNumber = document.querySelector('#num-inpt');
+    const form = document.querySelector('.task');
+    const items = document.querySelectorAll('.catalog-item');
+    const btnItems = document.querySelector('.catalog').querySelectorAll('button');
+    btnItems.forEach(el => {
+        el.addEventListener('click', event => {
+            form.classList.add('visible');
+            inptName.value = event.target.parentElement.children[0].textContent;
+            inptNumber.value = event.target.parentElement.children[1].textContent;
+        });
+    });
+    
+}
+
+request();
